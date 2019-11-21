d6317ddf5ec8270def5eaa9891e76dac8ff0fee1
CuongNC1612 <35983743+CuongNC1612@users.noreply.github.com>
2019-11-21 23:12:35 +0700

    Update check_IP_login_multiple_ID.sh

--- check_IP_login_multiple_ID.sh
+++ check_IP_login_multiple_ID.sh
@@ -71,5 +71,5 @@ if [ -s $RESULT_CHECK ]
 then
 cd $CUR_DIR/$YESTERDAY
 zip -P abc123adf result_check_ip_warn$YESTERDAY.zip check_ip_result.txt
-#cat ../mail_tmp.txt  | mailx -a "result_check_ip_warn$YESTERDAY.zip" -s " [$YESTERDAY][PROD][REPORT]Check account login via duplicate IP address" -S smtp=1.2.3.4 -S smtp-auth=login -S smtp-auth-user=noreply@uat-jp-st.localdomain -S smtp-auth-password=123@123a -S from="noreply@uat-jp-st.localdomain" abc@123.com
+#cat ../mail_tmp.txt  | mailx -a "result_check_ip_warn$YESTERDAY.zip" -s " [$YESTERDAY][PROD][REPORT]Check account login via duplicate IP address" -S smtp=1.2.3.4 -S smtp-auth=login -S smtp-auth-user=n<email>@uat-jp-st.localdomain -S smtp-auth-password=<password> -S from="noreply@uat-jp-st.localdomain" abc@123.com
 fi
